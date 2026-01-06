bbae71d8cfab0cc74f7dd9e8b25ca2e129bc498e
saloni <salonimishramishra9527@gmail.com>
2026-01-06 10:28:45 +0530

    first commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `hello ${person} , WELCOME TO CHAICODE`;
+}
+const username = "chai aur typescript";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+function greet (person : string): string {
+    return `hello ${person} , WELCOME TO CHAICODE`;
+
+}
+
+const username:string = "chai aur typescript";
+console.log(greet(username));
