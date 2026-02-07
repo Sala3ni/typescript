c9a13d2aba56d780c3b2af8ce20d6e1bbea5d36b
saloni <salonimishramishra9527@gmail.com>
2026-02-07 13:24:50 +0530

    Remove node_modules and dist from git tracking, update .gitignore

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function greet(person) {
-    return `hello ${person} , WELCOME TO CHAICODE`;
-}
-const username = "chai aur typescript";
-console.log(greet(username));
-//# sourceMappingURL=index.js.map
